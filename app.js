5bf631f82abf10a4bbec51b4899ba21487398f95
MihirSumant <mihirsumant@gmail.com>
2023-08-20 21:51:17 +0530

    Implemented onclick image change for all choices

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
-let rock = document.getElementById('Rock');
+const rock = document.getElementById('Rock');
 const paper = document.getElementById('Paper');
 const scissors = document.getElementById('Scissors');
-const playerChoiceImg = document.getElementById('playerChoice');
+let playerChoiceImg = document.getElementById('playerChoice');
 
 rock.onclick = () => {  
   paper.disabled = true;
   scissors.disabled = true;
+  playerChoiceImg.src = './resources/rock-hand.png';
+  playerChoiceImg.alt = 'hand emoji for rock';  
 }
 
 paper.onclick = () => {
   rock.disabled = true;
-  scissors.disabled = true;  
+  scissors.disabled = true;
+  playerChoiceImg.src = './resources/paper-hand.png';
+  playerChoiceImg.alt = 'hand emoji for paper';
 }
 
 scissors.onclick = () => {  
   paper.disabled = true;
   rock.disabled = true;
+  playerChoiceImg.src = './resources/scissors-hand.png';
+  playerChoiceImg.alt = 'hand emoji for scissors';
 }
